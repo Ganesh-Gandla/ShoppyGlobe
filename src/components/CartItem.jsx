bb5ec2016115ce7f8d1325807ef9726eb6c5e3bc
ganesh-gandla <ganeshgandla19@gmail.com>
2025-11-19 11:31:59 +0530

    Created Cart page and Cart Item component

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,9 @@
+function CartItem() {
+  return (
+    <div>
+      <h4>Cart Item</h4>
+      {/* Cart item details will go here */}
+    </div>
+  );
+}
+export default CartItem;
---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,12 @@
+import CartItem from "../components/CartItem";
+
+function CartPage() {
+  return (
+    <div>
+      <h2>Cart Page</h2>
+      <CartItem />
+      {/* Cart items will be displayed here */}
+    </div>
+  );
+}
+export default CartPage;
