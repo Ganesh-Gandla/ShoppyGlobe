46caaf2437247079d6d2484a34483e298415c52a
ganesh-gandla <ganeshgandla19@gmail.com>
2025-11-19 11:41:14 +0530

    Added Checkout Page to the project

---
+++ src/pages/CheckOut.jsx
@@ -0,0 +1,12 @@
+import CartItem from "../components/CartItem";
+
+function CheckOut (){
+    return(
+        <div>
+            <form action=""></form>
+            <CartItem />
+            <button>Place Order</button>
+        </div>
+    )
+}
+export default CheckOut;
