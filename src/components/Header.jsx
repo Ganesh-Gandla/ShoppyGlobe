c8fb99d7f9230eb9e958827e3a4773e74d30383b
ganesh-gandla <ganeshgandla19@gmail.com>
2025-11-19 11:17:12 +0530

    Added Header & Navbar components to the project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar";
+
+function Header() {
+  return (
+    <header>
+        <h1 className="title">Shoppy Globe</h1>
+        <NavBar />
+    </header>
+    )
+}
+export default Header;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <img src="cart-icon.png" alt="Cart" />
+    </nav>
+    )
+}
+export default NavBar;
