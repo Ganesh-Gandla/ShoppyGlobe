e638b55c2948a72c8c412e439c44464e6e285821
ganesh-gandla <ganeshgandla19@gmail.com>
2025-11-20 19:24:30 +0530

    Added a feature to cart icon to display number of cart items

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,47 @@
 import { useState } from "react";
 import "../styles/NavBar.css";
-
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function NavBar() {
   const [open, setOpen] = useState(false);
 
+  // Get cart items from Redux
+  const cartItems = useSelector((state) => state.cart.items);
+
+  // Calculate total quantity
+  const totalQuantity = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <nav className="navbar">
-      
-
-      {/* Hamburger icon */}
+      {/* Hamburger */}
       <div className="hamburger" onClick={() => setOpen(!open)}>
         <span></span>
         <span></span>
         <span></span>
       </div>
 
-      <ul className={open ? "nav-links open" : "nav-links"}>
+      {/* Nav links */}
+      <ul
+        className={open ? "nav-links open" : "nav-links"}
+        onClick={() => setOpen(!open)}
+      >
         <Link to={"/"}><li>Home</li></Link>
-        <Link to={"/"}><li>Products</li></Link>
+        <Link to={"/Products"}><li>Products</li></Link>
         <Link to={"/About"}><li>About</li></Link>
         <Link to={"/Contact"}><li>Contact</li></Link>
       </ul>
 
+      {/* Logo */}
       <h1 className="title">ShoppyGlobe</h1>
 
-      <Link to={"/CartPage"}><img src="./cart.png" alt="Cart" className="cart-icon" /></Link>
+      {/* Cart icon with badge */}
+      <Link to={"/CartPage"} className="cart-wrapper">
+        <img src="./cart.png" alt="Cart" className="cart-icon" />
+        {totalQuantity > 0 && (
+          <span className="cart-count">{totalQuantity}</span>
+        )}
+      </Link>
     </nav>
   );
 }
